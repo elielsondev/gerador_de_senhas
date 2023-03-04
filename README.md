ed1dfa2588390f47a38f1611cf2753dde4e6db34
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-03-03 22:03:48 -0300

    basic functionalities created and packages implemented

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Gerador de senhas
-Gerador de senha criado com HTML, CSS, JavaScript e o pacote nanoid
+Gerador de senha criado com HTML, CSS, JavaScript e o pacote nanoid.
+
+O Vite fornece ferramentas de desenvolvimento para nossa aplicação, como, por exemplo, um servidor de desenvolvimento (também comumente chamado de dev-server) e um bundler para compilação final da aplicação.
+
+Para utilizar o dev-server, basta executar o comando no terminal:
+```npx vite --open```
+
+
--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
-const body = document.querySelector('body');
+import { nanoid } from 'nanoid';
+
+const passwordBtn = document.querySelector('#passwordButton');
+const displayPassword = document.querySelector('#passwordH2');
+const inputDigit = document.querySelector('#digitInput');
+const paragraphNumberDigit = document.querySelector('#digitNumberP');
+
+setInterval(() => {
+  paragraphNumberDigit.innerHTML = inputDigit.value;
+}, 1);
+
+passwordBtn.addEventListener('click', () => {
+  const { value } = inputDigit;
+  
+  if (value < 1 || value > 36) {
+    value = 21;
+    passwordBtn.disabled = true;
+  }
+  
+  const randomPassword = nanoid(value);
+  displayPassword.innerHTML = randomPassword;
+});
